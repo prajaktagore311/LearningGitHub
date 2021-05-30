fe2e7cdc0f8d5800b13d7180be92613d0afa1854
Prajakta gore <prajaktagore311@gmail.com>
2021-05-31 00:05:53 +0530

    Add simple1 program

---
+++ simple1.java
@@ -0,0 +1,10 @@
+public class simple1{
+	public static void main(String args[])
+	{
+		System.out.println("Hello simple");
+		void display();
+	}
+	public void display()
+	{
+		sysytem.out.println("How are you");
+
