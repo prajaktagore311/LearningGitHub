e080aad78cab5bbaef0f0faa8f5a07ac9d4f9c89
Prajakta Gore <84570285+prajaktagore311@users.noreply.github.com>
2021-06-01 00:17:32 +0530

    Update simple1.java

--- simple1.java
+++ simple1.java
@@ -2,9 +2,9 @@ public class simple1{
 	public static void main(String args[])
 	{
 		System.out.println("Hello simple");
-		void display();
+		 display();
 	}
-	public void display()
+	public static void display()
 	{
 		sysytem.out.println("How are you");
-
+	}
