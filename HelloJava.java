8c3494b6b73ed39e48f36caf818f01466119e6d6
Prajakta gore <prajaktagore311@gmail.com>
2021-06-01 14:40:21 +0530

    Adding one java Program

---
+++ HelloJava.java
@@ -0,0 +1,16 @@
+package date06032021;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		display();
+
+	}
+	static void display() {
+		System.out.println("I am from dispaly method");
+	}
+
+}
+ 
